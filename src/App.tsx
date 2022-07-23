e74cc13822e2a63a5e344277bc7c5e959ae9a1d6
Saemi Hong <sstyle940104@gmail.com>
2022-07-23 16:03:10 +0900

    add: Add fetch data in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+// components
+import { Drawer, Progress, Grid, Badge } from 'antd';
+import { ShoppingCartOutlined } from "@ant-design/icons";
+
+//styles
+import { Wrapper } from "./App.styles";
+
+//types
+export type CartItemType = {
+    id: number;
+    title: string;
+    price: number;
+    category: string;
+    image: string;
+    description: string;
+    amount: number; //added customed info
+}
+
+const getProducts = async() => 
+    await(await fetch('https://fakestoreapi.com/products')).json(); //await for json
 const App = () => {
     return (
         <div className="App">
