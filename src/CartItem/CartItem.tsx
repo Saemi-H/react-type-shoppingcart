fba6fe32d8b4f2b240dacde44cb4da9fe134da93
Saemi Hong <sstyle940104@gmail.com>
2022-07-24 13:46:41 +0900

    feat: complete UI for CartItem in CartItem

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -12,6 +12,30 @@ type Props = {
     removeFromCart: (id: number) => void;
 }
 
-const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) => <div>cart item</div>
+const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) => (
+    <Wrapper>
+        <div>
+            <h3>{item.title}</h3>
+            <div className='information'>
+                <p>Price: ${item.price}</p>
+                <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+            </div>
+            <div className='buttons'>
+                <Button
+                    onClick={() => removeFromCart(item.id)}
+                >
+                   - 
+                </Button>
+                <p>{item.amount}</p>
+                <Button
+                    onClick={() => addToCart(item)}
+                >
+                   + 
+                </Button>
+            </div>
+        </div>
+        <img src={item.image} alt={item.title} />
+    </Wrapper>
+)
 
 export default CartItem;
