3f0caa8f84ec3fae127c20d61ace0b6109e3947a
Saemi Hong <sstyle940104@gmail.com>
2022-07-24 13:18:45 +0900

    feat: created initial CartItem component

---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+
+//types
+import { CartItemType } from '../App';
+
+//styles
+import { Wrapper } from './CartItem.styles';
+
+const CartItem: React.FC = () => <div>cart item</div>
+
+export default CartItem;
